f1176cdac95b6850c00e41ae4beb2a227c0ae60d
Kellie Mogg <kelliedmogg@gmail.com>
2021-05-03 16:34:23 +0000

    update with kellies versions

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-<<<<<<< HEAD
 """ displays a string based on route slash """
 
 
@@ -18,28 +17,3 @@ if __name__ == '__main__':
         return "HBNB"
 
     app.run(host='0.0.0.0', port='5000')
-=======
-""" this module contains a script that starts a Flask web application
-    the web application must be listening on 0.0.0.0, port 5000
-    Routes:
-    /
-    /hbnb
-"""
-from flask import Flask
-app = Flask(__name__)
-
-
-@app.route('/', strict_slashes=False)
-def hello_hbnb():
-    """ route function to return Hello HBNB! """
-    return 'Hello HBNB!'
-
-
-@app.route('/hbnb', strict_slashes=False)
-def hbnb_only():
-    """ route function to return HBNB """
-    return 'HBNB'
-
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
->>>>>>> 4e9d85e94116c80a4c0a4b6eee6d0ea8588cdc67
